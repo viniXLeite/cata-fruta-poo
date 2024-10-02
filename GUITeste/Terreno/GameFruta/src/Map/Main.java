d1c0c8236a60d450c83695651b7bf7353458e268
Antonio Francisco <academico.antonio09@gmail.com>
2024-10-01 22:08:29 -0300

    InicializaçãoTerreno/Mudanças

--- GUITeste/Terreno/GameFruta/src/Map/Main.java
+++ GUITeste/Terreno/GameFruta/src/Map/Main.java
@@ -4,28 +4,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
-        
-        System.out.println("Digite o número de linhas do terreno:");
-        int linhas = scanner.nextInt();
-        System.out.println("Digite o número de colunas do terreno:");
-        int colunas = scanner.nextInt();
-
-        CriarMapa terreno = new CriarMapa(linhas, colunas);
-
-        
-        for (int i = 0; i < linhas; i++) {
-            for (int j = 0; j < colunas; j++) {
-                System.out.println("Digite o tipo de chão para a posição [" + i + ", " + j + "] (rochoso/grama):");
-                String tipo = scanner.next();
-                terreno.adicionarChao(i, j, tipo);
-            }
-        }
-
-        // Exibir o terreno final
-        System.out.println("\nTerreno final:");
-        terreno.exibirTabuleiro();
+		
 
 	}
 
--- GUITeste/Terreno/GameFruta/src/Map/Terreno.java
+++ GUITeste/Terreno/GameFruta/src/Map/Terreno.java
@@ -1,47 +1,45 @@
 package Map;
+import java.util.Random;
+
 
 public class Terreno {
 	private Chao[][] tabuleiro;
 	private int linhas, colunas;
 	
-	public Terreno(int linhas, int colunas) {
-		this.linhas = linhas;
-		this.colunas = colunas;
+	public Terreno(int m, int numPedras) {
+		this.linhas = m;
+		this.colunas = m;
 		tabuleiro = new Chao[linhas][colunas];
 	}
 	
-	public void adicionarChao(int linha, int coluna, String tipo) {
-        if (linha < 0 || linha >= linhas || coluna < 0 || coluna >= colunas) {
-            System.out.println("Posição inválida!");
-            return;
-        }
-
-        switch (tipo.toLowerCase()) {
-            case "rochoso":
-                tabuleiro[linha][coluna] = new ChaoRochoso();
-                break;
-            case "grama":
-                tabuleiro[linha][coluna] = new ChaoGramado();
-                break;
-            default:
-                System.out.println("Tipo de chão inválido!");
-                return;
-        }
-        System.out.println("Chão do tipo " + tipo + " adicionado na posição [" + linha + ", " + coluna + "].");
-    }
-	
-	public void exibirTabuleiro(){
+	private void inicializarTerreno(int numPedras) {
+		Random random = new Random();
+		int pedrasColocadas = 0;
+		
+		//Gera posições aleatorias no terreno para  colocar as pedras definidas pelo usuário
+		while(pedrasColocadas < numPedras) {
+			int linhaAleatoria = random.nextInt(linhas);
+			int colunaAleatoria = random.nextInt(colunas);
+			
+			if(!(tabuleiro[linhaAleatoria][colunaAleatoria] instanceof ChaoRochoso)) {
+				tabuleiro[linhaAleatoria][colunaAleatoria] = new ChaoRochoso();
+				pedrasColocadas++;
+			}
+			
+		}
+		
+		//Vejo quais posições não estão com rocha e coloco grama, como pede a professora
 		for(int i = 0; i < linhas; i++) {
 			for(int j = 0; j < colunas; j++) {
-				if (tabuleiro[i][j] != null) {
-                    System.out.print(tabuleiro[i][j].getTipo() + " | ");
-                } else {
-                    System.out.print("Vazio | ");
-                }
-            System.out.println();
+				if(!(tabuleiro[i][j] instanceof ChaoRochoso)) {
+					tabuleiro[i][j] = new ChaoGramado();
+				}
 			}
 		}
+		
 	}
+	
+	
 	
 	
 }
